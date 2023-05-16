49611a1d6a5d4f19c7846fb275bf1325b36c0e4f
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2023-05-16 19:12:24 -0300

    configured liquibase and h2 db. resolves #1

---
+++ src/main/java/dev/bastida/finca/core/FincaCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.bastida.finca.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bastida/finca/core/FincaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bastida.finca.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
