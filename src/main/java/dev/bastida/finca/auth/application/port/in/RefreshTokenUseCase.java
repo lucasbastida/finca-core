f571b75177feef4255e8e0de54c089af21a590e4
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2023-06-01 23:36:53 -0300

    Removed constructor validation in RefreshTokenUseCase

--- src/main/java/dev/bastida/finca/auth/application/port/in/RefreshTokenUseCase.java
+++ src/main/java/dev/bastida/finca/auth/application/port/in/RefreshTokenUseCase.java
@@ -1,16 +1,9 @@
 package dev.bastida.finca.auth.application.port.in;
 
-import org.springframework.util.StringUtils;
-
 public interface RefreshTokenUseCase {
     RefreshTokenResponse refresh(RefreshTokenCommand command);
 
     record RefreshTokenCommand(String refreshToken) {
-        public RefreshTokenCommand {
-            if (!StringUtils.hasText(refreshToken)) {
-                throw new IllegalArgumentException("refreshToken must not be empty");
-            }
-        }
     }
 
     record RefreshTokenResponse(String accessToken, String refreshToken) {
